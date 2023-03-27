8abc78fa16dde0e8bb8d8eb3417f1927b651c97d
Chamindu Wanigasekara <chaminduwanigasekara@gmail.com>
2023-03-27 11:35:31 +0530

    add access to the local music
    user can import songs from their own computers . developed by using database and php.

--- authentication/authorization_code/web/script.js
+++ authentication/authorization_code/web/script.js
@@ -1,31 +1,43 @@
 let now_playing = document.querySelector('.now-playing');
-let track_art = document.querySelector('.track-art');
-let track_name = document.querySelector('.track-name');
-let track_artist = document.querySelector('.track-artist');
-
+let music_art = document.querySelector('.music_art');
+let music_name = document.querySelector('.music-name');
+let music_artist = document.querySelector('.music-artist');
 let playpause_btn = document.querySelector('.playpause-track');
+let previous_btn = document.querySelector('.prev-track');
 let next_btn = document.querySelector('.next-track');
-let prev_btn = document.querySelector('.prev-track');
-
 let seek_slider = document.querySelector('.seek_slider');
 let volume_slider = document.querySelector('.volume_slider');
-let curr_time = document.querySelector('.current-time');
+let current_time = document.querySelector('.current-time');
 let total_duration = document.querySelector('.total-duration');
 let wave = document.getElementById('wave'); 
 let randomIcon = document.querySelector('.fa-random');
-let curr_track = document.createElement('audio');
+let current_track = document.createElement('audio');
 
-let track_index = 0;
-let isPlaying = false;
+
+let musicIndex = 0;
 let isRandom = false;
+let isPlaying = false;
 let updateTimer;
 
-const music_list = [
+
+const Songs_list = [
     {
-        img : 'images/stay.png',
+        img: "images/Hardwind.jpg",
+        name: "Hardwind Want Me",
+        artist: "Mike Archangelo",
+        music : "music/Hardwind.mp3"
+    },
+    {
+        img: 'images/stay.png',
         name : 'Stay',
         artist : ' Justin Bieber',
         music : 'music/stay.mp3'
+    },
+    {
+        img: 'images/sun.jpg',
+        name: "Sun Goes Down",
+        artist: "Jim Yosef x Roy",
+        music : "music/Sun Goes Down.mp3"
     },
     {
         img : 'images/fallingdown.jpg',
@@ -34,10 +46,22 @@ const music_list = [
         music : 'music/fallingdown.mp3'
     },
     {
+        img: "images/Anywhere.jpg",
+        name: "Ikson Anywhere ",
+        artist: "Audio Library",
+        music : "music/Ikson Anywhere.mp3"
+    },
+    {
         img : 'images/faded.png',
         name : 'Faded',
         artist : 'Alan Walker',
         music : 'music/Faded.mp3'
+    },
+    {
+        img: 'images/Jvna.jpg',
+        name: "Crazy",
+        artist: "Beauz & Jvna",
+        music : "music/Beauz & Jvna.mp3"
     },
     {
         img : 'images/ratherbe.jpg',
@@ -46,39 +70,54 @@ const music_list = [
         music : 'music/Rather Be.mp3'
     },
     {
+        img: "images/LostSky.jpg",
+        name: "Vision NCS",
+        artist: "NCS Release",
+        music : "music/Lost Sky.mp3"
+    },
+    {
         img : 'images/lotus.jpg',
         name : 'Aniyam',
         artist : 'King Lotus',
         music : 'music/Aniyam - King Lotuss.mp3'
+    },
+    {
+        img: "images/HarleyBird.jpg",
+        name: "Harley Bird Home",
+        artist: "Jordan Schor",
+        music : "music/Harley Bird.mp3"
     }
 ];
 
-loadTrack(track_index);
+loadTrack(musicIndex);
 
-function loadTrack(track_index){
+function loadTrack(musicIndex){
     clearInterval(updateTimer);
     reset();
 
-    curr_track.src = music_list[track_index].music;
-    curr_track.load();
+    current_track.src = Songs_list[musicIndex].music;
+    current_track.load();
 
-    track_art.style.backgroundImage = "url(" + music_list[track_index].img + ")";
-    track_name.textContent = music_list[track_index].name;
-    track_artist.textContent = music_list[track_index].artist;
-    now_playing.textContent = "Playing music " + (track_index + 1) + " of " + music_list.length;
+    music_art.style.backgroundImage = "url(" + Songs_list[musicIndex].img + ")";
+    music_name.textContent = Songs_list[musicIndex].name;
+    music_artist.textContent = Songs_list[musicIndex].artist;
+    now_playing.textContent = "Playing music " + (musicIndex + 1) + " of " + Songs_list.length;
 
     updateTimer = setInterval(setUpdate, 1000);
 
-    curr_track.addEventListener('ended', nextTrack);
+    current_track.addEventListener('ended', nextTrack);
     random_bg_color();
 }
 
 
 function reset(){
-    curr_time.textContent = "00:00";
+    current_time.textContent = "00:00";
     total_duration.textContent = "00:00";
     seek_slider.value = 0;
+    pauseTrack();
+    
 }
+
 function randomTrack(){
     isRandom ? pauseRandom() : playRandom();
 }
@@ -91,7 +130,7 @@ function pauseRandom(){
     randomIcon.classList.remove('randomActive');
 }
 function repeatTrack(){
-    let current_index = track_index;
+    let current_index = musicIndex;
     loadTrack(current_index);
     playTrack();
 }
@@ -99,64 +138,66 @@ function playpauseTrack(){
     isPlaying ? pauseTrack() : playTrack();
 }
 function playTrack(){
-    curr_track.play();
+    current_track.play();
     isPlaying = true;
-    track_art.classList.add('rotate');
+    music_art.classList.add('rotate');
     wave.classList.add('loader');
     playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
 }
 function pauseTrack(){
-    curr_track.pause();
+    current_track.pause();
     isPlaying = false;
-    track_art.classList.remove('rotate');
+    music_art.classList.remove('rotate');
     wave.classList.remove('loader');
     playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
 }
 function nextTrack(){
-    if(track_index < music_list.length - 1 && isRandom === false){
-        track_index += 1;
-    }else if(track_index < music_list.length - 1 && isRandom === true){
-        let random_index = Number.parseInt(Math.random() * music_list.length);
-        track_index = random_index;
+    if(musicIndex < Songs_list.length - 1 && isRandom === false){
+        musicIndex += 1;
+    }else if(musicIndex < Songs_list.length - 1 && isRandom === true){
+        let random_index = Number.parseInt(Math.random() * Songs_list.length);
+        musicIndex = random_index;
     }else{
-        track_index = 0;
+        musicIndex = 0;
     }
-    loadTrack(track_index);
+    loadTrack(musicIndex);
     playTrack();
 }
 function prevTrack(){
-    if(track_index > 0){
-        track_index -= 1;
+    if(musicIndex > 0){
+        musicIndex -= 1;
     }else{
-        track_index = music_list.length -1;
+        musicIndex = Songs_list.length -1;
     }
-    loadTrack(track_index);
+    loadTrack(musicIndex);
     playTrack();
 }
 function seekTo(){
-    let seekto = curr_track.duration * (seek_slider.value / 100);
-    curr_track.currentTime = seekto;
+    let seekto = current_track.duration * (seek_slider.value / 100);
+    current_track.currentTime = seekto;
 }
 function setVolume(){
-    curr_track.volume = volume_slider.value / 100;
+    current_track.volume = volume_slider.value / 100;
 }
 function setUpdate(){
     let seekPosition = 0;
-    if(!isNaN(curr_track.duration)){
-        seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+    if(!isNaN(current_track.duration)){
+        seekPosition = current_track.currentTime * (100 / current_track.duration);
         seek_slider.value = seekPosition;
 
-        let currentMinutes = Math.floor(curr_track.currentTime / 60);
-        let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
-        let durationMinutes = Math.floor(curr_track.duration / 60);
-        let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+        let currentMinutes = Math.floor(current_track.currentTime / 60);
+        let currentSeconds = Math.floor(current_track.currentTime - currentMinutes * 60);
+        let durationMinutes = Math.floor(current_track.duration / 60);
+        let durationSeconds = Math.floor(current_track.duration - durationMinutes * 60);
 
         if(currentSeconds < 10) {currentSeconds = "0" + currentSeconds; }
         if(durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
         if(currentMinutes < 10) {currentMinutes = "0" + currentMinutes; }
         if(durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
 
-        curr_time.textContent = currentMinutes + ":" + currentSeconds;
+        current_time.textContent = currentMinutes + ":" + currentSeconds;
         total_duration.textContent = durationMinutes + ":" + durationSeconds;
     }
 }
+
+
